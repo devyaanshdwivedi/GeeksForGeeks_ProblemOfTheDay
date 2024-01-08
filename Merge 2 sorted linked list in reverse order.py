d3032f7fc17d22064ce24b36ad199fadab2992d2
Devyaansh Dwivedi <91078095+devyaanshdwivedi@users.noreply.github.com>
2024-01-08 22:22:10 +0530

    Day_34

---
+++ Merge 2 sorted linked list in reverse order.py
@@ -0,0 +1,96 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+
+class Solution:
+    def mergeResult(self, h1, h2):
+        #return head of merged list
+        def link_list(head):
+            current = head
+            result = []
+            while current is not None:
+                result.append(current.data)
+                current = current.next
+            return result
+        list1 = link_list(h1)
+        list2 = link_list(h2)
+        mer_list = list1+list2
+        mer_list.sort(reverse = True)
+        if not mer_list:
+            return None
+
+        # Create the head of the linked list
+        head = Node(mer_list[0])
+        current = head
+    
+        # Iterate through the list and create nodes for each element
+        for value in mer_list[1:]:
+            current.next = Node(value)
+            current = current.next
+    
+        return head
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Llist:
+    def __init__(self):
+        self.head=None
+    
+    def insert(self,data,tail):
+        node=Node(data)
+        
+        if not self.head:
+            self.head=node
+            return node
+        
+        tail.next=node
+        return node
+        
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        
+        n1,n2=[int(x) for x in input().split()]
+        
+        arr1=[int(x) for x in input().split()]
+        ll1=Llist()
+        tail=None
+        for nodeData in arr1:
+            tail=ll1.insert(nodeData,tail)
+            
+            
+        arr2=[int(x) for x in input().split()]
+        ll2=Llist()
+        tail=None
+        for nodeData in arr2:
+            tail=ll2.insert(nodeData,tail)
+        
+        
+        ob = Solution()
+        resHead=ob.mergeResult(ll1.head,ll2.head)
+        printList(resHead)
+        print()
+        
+    
+    
+# } Driver Code Ends
